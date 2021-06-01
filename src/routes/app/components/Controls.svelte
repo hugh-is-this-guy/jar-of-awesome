430e39aa7ce0559374f3566103f46ed15121e91e
Hugh Brace <hughbrace@gmail.com>
2021-06-01 16:01:45 +0700

    Modal and Controls in separeate files

---
+++ src/routes/app/components/Controls.svelte
@@ -0,0 +1,48 @@
+<script>
+  import { showModal } from "../stores.js"
+
+  import Icon from 'svelte-awesome/components/Icon.svelte'
+  import { cog, plus } from 'svelte-awesome/icons';
+</script>
+
+<style>
+  div {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+
+    width: 100%;
+    padding: 16px;
+    box-sizing: border-box;
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    height: 46px;
+    width: 46px;
+
+    border-radius: 50%;
+    padding: 0;
+    border: none;
+    background: none;
+    background-color: white;
+  }
+</style>
+
+<div class="controls">
+  <button on:click={() => $showModal = "user"}>
+    <Icon data={cog} scale={2} />
+  </button>
+
+  <button on:click={() => $showModal = "new"}>
+    <Icon data={plus} scale={2} />
+  </button>
+</div>
---
+++ src/routes/app/components/Modal.svelte
@@ -0,0 +1,48 @@
+<script>
+  import { showModal } from "../stores.js"
+
+  import New from "./New.svelte"
+  import Icon from 'svelte-awesome/components/Icon.svelte'
+  import { times } from 'svelte-awesome/icons';
+</script>
+
+<style>
+  div {
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    width: 100vw;
+    height: 100vh;
+
+    background-color: white;
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    height: 46px;
+    width: 46px;
+
+    border-radius: 50%;
+    padding: 0;
+    border: none;
+    background: none;
+    background-color: white;
+
+    position: absolute;
+    bottom: 16px;
+    right: 16px;
+  }
+</style>
+
+{#if $showModal}
+  <div>
+    <New />
+    <button on:click={() => $showModal = false}>
+      <Icon data={times} scale={2} />
+    </button>
+  </div>
+{/if}
